7dfbc2be6a08182b9d7556b3217d2ec51867bbe4
Toni <119048137+TonyZhechev@users.noreply.github.com>
2024-11-09 00:43:15 +0200

    Add files via upload
    exercise added on 9.11.24

---
+++ PythonBasics/Enumeration2.py
@@ -0,0 +1,3 @@
+products = ['table', 'chair', 'door']
+for index,item in enumerate(products):
+     print('Product:',item)
---
+++ PythonBasics/WrongPassword.py
@@ -0,0 +1,6 @@
+correct_password = '123'
+password = input('Enter the password:')
+if password == correct_password:
+    print('correct password')
+else:
+    print('Wrong answer')
