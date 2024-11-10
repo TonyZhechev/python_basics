fd833bbb31b2b49c44ed017740224ad1e3b1b8f6
Toni <119048137+TonyZhechev@users.noreply.github.com>
2024-11-10 10:20:15 +0200

    Add files via upload

--- PythonBasics/For-Loop and Sorted List.py
+++ PythonBasics/For-Loop and Sorted List.py
@@ -1,3 +1,5 @@
 measurements = [177.8, 175.8, 166.9, 182.5]
-for i in enumerate(measurements):
-    print(i)
+measurements.sort(reverse=True)
+for item in measurements:
+    measurements = f'{item}'
+    print(measurements)
