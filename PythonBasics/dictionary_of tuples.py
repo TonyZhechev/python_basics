0395459f91d895f548fbb06d59764a624316a37f
Toni <119048137+TonyZhechev@users.noreply.github.com>
2024-12-05 17:52:04 +0200

    Add files via upload

---
+++ PythonBasics/dictionary_of tuples.py
@@ -0,0 +1,8 @@
+# dictionary ot tuples
+day_temperatures =  {"Morning":(2.2,3.2,2.6),"noon":(1.2,45,3.76),"evening":(12.3,15.2,1.2)}
+print(day_temperatures)
+
+
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+print(letters[:3])
